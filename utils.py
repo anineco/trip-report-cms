4494e0af2c2632ffca8317ae636feafa78e70d0e
Anineco Nyanta <anineco@nifty.com>
2025-01-16 05:45:13 +0900

    Consistent use of quotation marks

--- utils.py
+++ utils.py
@@ -11,31 +11,31 @@ def iso_period(s, e): # datetime object
         end = e.strftime('%d')
     else:
         return start
-    return f"{start}/{end}"
+    return f'{start}/{end}'
 
 def jp_period(s, e): # datetime object
     """Return a string of period in Japanese format."""
-    start = f"{s.year}年{s.month}月{s.day}日"
+    start = f'{s.year}年{s.month}月{s.day}日'
     if s.year != e.year:
-        end = f"{e.year}年{e.month}月{e.day}日"
+        end = f'{e.year}年{e.month}月{e.day}日'
     elif s.month != e.month:
-        end = f"{e.month}月{e.day}日"
+        end = f'{e.month}月{e.day}日'
     elif s.day != e.day:
-        end = f"{e.day}日"
+        end = f'{e.day}日'
     else:
         return start
-    return f"{start}〜{end}"
+    return f'{start}〜{end}'
 
 def jp_period_short(s, e): # datetime object
     """Return a string of period in Japanese short format."""
-    start = f"{s.month}月{s.day}日"
+    start = f'{s.month}月{s.day}日'
     if s.year != e.year or s.month != e.month:
-        end = f"{e.month}月{e.day}日"
+        end = f'{e.month}月{e.day}日'
     elif s.day != e.day:
-        end = f"{e.day}日"
+        end = f'{e.day}日'
     else:
         return start
-    return f"{start}〜{end}"
+    return f'{start}〜{end}'
 
 days = ['月', '火', '水', '木', '金', '土', '日']
 
@@ -43,15 +43,15 @@ def jp_datespan(s, e): # datetime object
     """Return a string of date span in Japanese format."""
     ws = days[s.weekday()]
     we = days[e.weekday()]
-    start = f"{s.year}年{s.month}月{s.day}日（{ws}）"
+    start = f'{s.year}年{s.month}月{s.day}日（{ws}）'
     if s.year != e.year:
-        end = f"{e.year}年{e.month}月{e.day}日（{we}）"
+        end = f'{e.year}年{e.month}月{e.day}日（{we}）'
     elif s.month != e.month:
-        end = f"{e.month}月{e.day}日（{we}）"
+        end = f'{e.month}月{e.day}日（{we}）'
     elif s.day != e.day:
-        end = f"{e.day}日（{we}）"
+        end = f'{e.day}日（{we}）'
     else:
-        end = ""
+        end = ''
     return start, end
 
 # __END__
