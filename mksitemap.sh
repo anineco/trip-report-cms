52cefc75124dd6b6d2b9ab231323a9f19fdf7ebd
Anineco Nyanta <anineco@nifty.com>
2025-09-20 16:46:31 +0900

    use stat instead of ls

--- mksitemap.sh
+++ mksitemap.sh
@@ -1,16 +1,17 @@
 #!/bin/sh
 echo '<?xml version="1.0" encoding="UTF-8"?>'
 echo '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">'
-/bin/ls -l -T *.html| while read p l u g s m d t y n; do
-  case $n in
+for name in *.html; do
+  lastmod=$(stat -c %y "$name" | cut -d' ' -f1)
+  case $name in
   [0-9]*)
-    printf "<url>\n <loc>https://anineco.org/%s</loc>\n <lastmod>%04d-%02d-%02d</lastmod>\n <priority>0.8</priority>\n</url>\n" $n $y $m $d
+    printf "<url>\n <loc>https://anineco.org/%s</loc>\n <lastmod>%s</lastmod>\n <priority>0.8</priority>\n</url>\n" $name $lastmod
     ;;
   index.html)
-    printf "<url>\n <loc>https://anineco.org/</loc>\n <lastmod>%04d-%02d-%02d</lastmod>\n <priority>0.5</priority>\n</url>\n" $y $m $d
+    printf "<url>\n <loc>https://anineco.org/</loc>\n <lastmod>%s</lastmod>\n <priority>0.5</priority>\n</url>\n" $lastmod
     ;;
   about.html|link.html|profile.html|nandoku.html)
-    printf "<url>\n <loc>https://anineco.org/%s</loc>\n <lastmod>%04d-%02d-%02d</lastmod>\n <priority>0.2</priority>\n</url>\n" $n $y $m $d
+    printf "<url>\n <loc>https://anineco.org/%s</loc>\n <lastmod>%s</lastmod>\n <priority>0.2</priority>\n</url>\n" $name $lastmod
     ;;
   *)
     ;;
