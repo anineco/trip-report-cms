d09b31764e40514d4c048292548f5702708272de
Anineco Nyanta <anineco@nifty.com>
2025-10-22 22:09:58 +0900

    query by id

--- share/mt.php
+++ share/mt.php
@@ -1,59 +1,138 @@
 <?php
-$home = '/home/anineco'; # 🔖 user's home directory
-$cf = parse_ini_file($home . '/.my.cnf'); # MySQL configuration
+$home = '/home/anineco'; # CONFIG: user's home directory
+$cf = parse_ini_file($home . '/.my.cnf');
 $dsn = "mysql:dbname=$cf[database];host=$cf[host];charset=utf8mb4;readOnly=1";
-$dbh = new PDO($dsn, $cf['user'], $cf['password']);
-
-# 位置の許容誤差（ST_Buffer の半径）
-$sql = <<<'EOS'
-SET @EPS=IF(0/*!80003 +1 */,40,0.00036) -- [m] or [deg]
-EOS;
-$dbh->exec($sql);
-
-$lat = filter_input(INPUT_GET, 'lat', FILTER_VALIDATE_FLOAT, [
-    'options' => ['min_range' => -90, 'max_range' => 90]
-]);
-$lon = filter_input(INPUT_GET, 'lon', FILTER_VALIDATE_FLOAT, [
-    'options' => [ 'min_range' => -180, 'max_range' => 180]
-]);
-if (!isset($lat, $lon) || $lat === false || $lon === false) {
-    http_response_code(400); # Bad Request
-    $dbh = null;
+$options = [
+    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    PDO::ATTR_EMULATE_PREPARES => false,
+];
+try {
+    $dbh = new PDO($dsn, $cf['user'], $cf['password'], $options);
+} catch (PDOException $e) {
+    http_response_code(500); # Internal Server Error
     exit;
 }
-$sql = <<<'EOS'
-SET @g=ST_GeomFromText(?,4326/*!80003, 'axis-order=long-lat' */)
-EOS;
-$sth = $dbh->prepare($sql);
-$sth->bindValue(1, "POINT($lon $lat)");
-$sth->execute();
-$sth = null;
-#
-# 最寄りの山名
-#
 
-# NOTE: ST_Distance_Sphere() is not available in MySQL 5.7, use ST_Distance() instead
-$sql = <<<'EOS'
-SELECT id,name,ST_Distance_Sphere(pt,@g) AS distance FROM geom
-WHERE ST_Within(pt,ST_Buffer(@g,@EPS))
-ORDER BY distance LIMIT 1
-EOS;
-$sth = $dbh->query($sql);
-$output = $sth->fetchAll(PDO::FETCH_ASSOC);
-$sth = null;
-if ($output) {
-    $id = $output[0]['id'];
+# Usage(example):
+#   mt.php?id=396
+# or
+#   mt.php?lat=35.360277&lon=138.727227
+
+$id = filter_input(INPUT_GET, 'id', FILTER_VALIDATE_INT, [
+    'options' => ['min_range' => 1]
+]);
+if (!isset($id) || $id === false) {
     #
-    # 所属都道府県
+    # search nearest mountain by latitude/longitude
     #
+    $lat = filter_input(INPUT_GET, 'lat', FILTER_VALIDATE_FLOAT, [
+        'options' => ['min_range' => -90, 'max_range' => 90]
+    ]);
+    $lon = filter_input(INPUT_GET, 'lon', FILTER_VALIDATE_FLOAT, [
+        'options' => [ 'min_range' => -180, 'max_range' => 180]
+    ]);
+    if (!isset($lat, $lon) || $lat === false || $lon === false) {
+        $dbh = null;
+        http_response_code(400); # Bad Request
+        exit;
+    }
+
+    # CONFIG: set search radius (meters)
     $sql = <<<'EOS'
-SELECT code, name FROM city WHERE code IN (SELECT DISTINCT (code DIV 1000) * 1000 FROM location WHERE id=?) ORDER BY code
+SET @EPS=100
+EOS;
+    $dbh->exec($sql);
+
+    # set center point @g
+    # NOTE: 'axis-order=long-lat' is required for MySQL 8 and later
+    $sql = <<<'EOS'
+SET @g=ST_GeomFromText(?, 4326, 'axis-order=long-lat')
 EOS;
     $sth = $dbh->prepare($sql);
-    $sth->bindValue(1, $id, PDO::PARAM_INT);
-    $sth->execute();
-    $output[0]['prefectures'] = $sth->fetchAll(PDO::FETCH_ASSOC);
+    $sth->execute(["POINT($lon $lat)"]);
     $sth = null;
+
+    # find nearest mountain within the search radius
+    # NOTE: ST_Distance_Sphere() is available at MySQL 8 and later
+    $sql = <<<EOS
+SELECT id, s.kana, s.name, alt, lat, lon,
+    FORMAT(ST_Distance_Sphere(pt, @g), 1) AS distance
+FROM geom
+JOIN sanmei AS s USING (id)
+WHERE ST_Within(pt, ST_Buffer(@g, @EPS)) AND type<=1
+ORDER BY distance, type
+LIMIT 1
+EOS;
+    $sth = $dbh->query($sql);
+    $output = $sth->fetch();
+    $sth = null;
+} else {
+    #
+    # search mountain by ID
+    #
+    $sql = <<<'EOS'
+SELECT id, s.kana, s.name, alt, lat, lon,
+    '0.0' AS distance
+FROM geom
+JOIN sanmei AS s USING (id)
+WHERE id=? AND type<=1
+ORDER BY type
+LIMIT 1
+EOS;
+    $sth = $dbh->prepare($sql);
+    $sth->execute([$id]);
+    $output = $sth->fetch();
+    $sth = null;
+}
+
+if ($output) {
+    $id = $output['id'];
+    #
+    # add aliases
+    #
+    $sql = <<<'EOS'
+SELECT kana, name
+FROM sanmei
+WHERE id=? AND type>1
+EOS;
+    $sth = $dbh->prepare($sql);
+    $sth->execute([$id]);
+    $output['aliases'] = $sth->fetchAll();
+    $sth = null;
+    #
+    # add prefectures
+    #
+    $sql = <<<'EOS'
+SELECT code, name, qid
+FROM city
+WHERE code IN (
+    SELECT DISTINCT CONCAT(LEFT(code, 2), '000')
+    FROM location
+    WHERE id=?
+)
+ORDER BY code
+EOS;
+    $sth = $dbh->prepare($sql);
+    $sth->execute([$id]);
+    $output['prefectures'] = $sth->fetchAll();
+    $sth = null;
+    #
+    # add municipalities
+    #
+    $sql = <<<'EOS'
+SELECT code, name, qid
+FROM city
+JOIN location USING (code)
+WHERE id=?
+ORDER BY code
+EOS;
+    $sth = $dbh->prepare($sql);
+    $sth->execute([$id]);
+    $output['municipalities'] = $sth->fetchAll();
+    $sth = null;
+} else {
+    $output = new stdClass();
 }
 $dbh = null;
 header('Content-Type: application/json; charset=UTF-8');
